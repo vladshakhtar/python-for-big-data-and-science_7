f3f89d186f58da2d20c5be68f47d6c612d60329e
vladshakhtar <stolyarovs2003@gmail.com>
2024-03-20 00:56:59 +0200

    created 5 empty functions in input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+
+
+def enter_text_from_console():
+    pass
+
+def read_from_file(file_path):
+    pass
+
+def read_from_file_with_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,7 @@
+
+
+def output_text_to_console(text):
+    pass
+
+def write_to_file(file_path, data):
+    pass
